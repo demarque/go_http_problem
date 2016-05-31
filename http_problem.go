aaaecf783a4936788b21f72d8b88f9f857249d79
Bastien Quelen <bastien.quelen@feedbooks.com>
2016-05-31 17:19:41 +0200

    very basic version for internal need

---
+++ http_problem.go
@@ -0,0 +1,32 @@
+package httpproblem
+
+import "encoding/json"
+
+// https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-00
+
+// Problem define the struct for http-problem spec
+type Problem struct {
+	Type   string `json:"type"`
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+	Status int    `json:"status"`
+}
+
+// New create problem struct
+func New(problemType string, title string, detail string, status int) Problem {
+	var problem Problem
+
+	problem.Type = problemType
+	problem.Title = title
+	problem.Detail = detail
+	problem.Status = status
+
+	return problem
+}
+
+// Generate build final json
+func (problem *Problem) Generate() ([]byte, error) {
+	data, _ := json.Marshal(problem)
+
+	return data, nil
+}
