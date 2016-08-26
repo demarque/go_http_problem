ce9196e185dbf64b28361c1800107856128c2c4c
Bastien Quelen <bastien.quelen@feedbooks.com>
2016-08-26 16:11:09 +0200

    use error interface and clean public variable

--- http_problem.go
+++ http_problem.go
@@ -6,22 +6,22 @@ import "encoding/json"
 
 // Problem define the struct for http-problem spec
 type Problem struct {
-	Type   string `json:"type"`
-	Title  string `json:"title"`
-	Detail string `json:"detail"`
-	Status int    `json:"status"`
+	typeProblem string `json:"type"`
+	title       string `json:"title"`
+	detail      string `json:"detail"`
+	status      int    `json:"status"`
 }
 
 // New create problem struct
-func New(problemType string, title string, detail string, status int) Problem {
+func New(problemType string, title string, detail string, status int) *Problem {
 	var problem Problem
 
-	problem.Type = problemType
-	problem.Title = title
-	problem.Detail = detail
-	problem.Status = status
+	problem.typeProblem = problemType
+	problem.title = title
+	problem.detail = detail
+	problem.status = status
 
-	return problem
+	return &problem
 }
 
 // Generate build final json
@@ -34,4 +34,9 @@ func (problem *Problem) Generate() ([]byte, error) {
 // GetMediaType return the media type string
 func (problem *Problem) GetMediaType() string {
 	return "application/api-problem+json"
+}
+
+// Error show error from title to adher to error interface
+func (problem *Problem) Error() string {
+	return problem.title
 }
